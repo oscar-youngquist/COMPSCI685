8cfc9c58d697b190a52fa848c1b5784853772693
Adam Rivelli <arivelli@umass.edu>
2021-11-05 14:08:40 -0400

    paraphrase dataset using a en-fr-en backtranslation

--- Evaluation_Framework/utils/data_augmentation.py
+++ Evaluation_Framework/utils/data_augmentation.py
@@ -1,62 +1,32 @@
-from typing import List
-from googletrans import Translator
-from nltk.tokenize import sent_tokenize
+from transformers import pipeline
+from tqdm import tqdm
+import csv
+from pathlib import Path
+
+en_fr = pipeline("translation_en_to_fr", model="Helsinki-NLP/opus-mt-en-fr", device=0)
+fr_en = pipeline("translation_fr_to_en", model="Helsinki-NLP/opus-mt-fr-en", device=0)
+
+paraphrase = lambda doc: fr_en(en_fr(doc)[0]['translation_text'])[0]["translation_text"]
+
+if __name__ == "__main__":
+    path = Path(__file__).absolute().parent.parent.parent / "SubSumE_Data"
+    import pdb; pdb.set_trace()
 
 
-default_languages = [
-    'cs', # czech
-    'de', # german
-    'es', # spanish
-    'fi', # finnish
-    'fr', # french
-    'hi', # hindi
-    'it', # italian
-    'ja', # japanese
-    'pt', # portuguese
-    'ru', # russian
-    'vi', # vietnamese
-    'zh-cn',  # chinese
-]
+    with open(path / 'processed_state_sentences.csv', 'r') as infile:
+        with open(path / 'paraphrased_state_sentences.csv', 'w') as outfile:
+            reader = csv.DictReader(infile)
+            writer = csv.DictWriter(outfile, reader.fieldnames)
+            writer.writeheader()
+            i = 0
+            for row in tqdm(reader):
+                try:
+                    row['sentence'] = paraphrase(row['sentence'])
+                    writer.writerow(row)
+                except Exception as e:
+                    print(f"Failed to write sentence {row['sid']}")
+                    row['sentence'] = '<ERR>'
+                    writer.writerow(row)
 
 
-class TranslationAugmentor:
-    def __init__(self, languages: List[str]=default_languages):
-        self.translator = Translator()
-        self.languages = languages
-
-    def paraphrase_sentence(self, sentence, language):
-        translated = self.translator.translate(sentence, src='en', dest=language).text
-        return self.translator.translate(translated, src=language, dest='en').text
-
-    def paraphrase_document(self, doc: str) -> List[str]:
-        """
-        Args:
-            article (str): The entire article in a single string
-
-        Returns:
-            List[str]: List of single-string paraphrased articles 
-                       (not including the original article)
-        """
-        pivots = self.languages
-
-        paraphrases = []
-        tokenized = sent_tokenize(doc)
-        for language in pivots:
-            # The following lines should work (without iteration), but the googletrans
-            # library doesn't like being passed a list
-            # translated = self.translator.translate(tokenized, src='en', dest=language)
-            # backtranslated = " ".join(self.translator.translate(translated, src=language, dest='en'))
-
-            backtranslated = " ".join([self.paraphrase_sentence(s, language) for s in tokenized])
-            paraphrases.append(backtranslated[:-1])
-        return paraphrases
-
-    def paraphrase_documents(self, docs: List[str]) -> List[List[str]]:
-        """
-        Args:
-            docs (List[str]): Each string in the list is a complete document (summary or article)
-        """
-        paraphrased_docs = []
-        for doc in docs:
-            paraphrased_docs.append(self.back_translate_document(doc))
-        return paraphrased_docs
+        
